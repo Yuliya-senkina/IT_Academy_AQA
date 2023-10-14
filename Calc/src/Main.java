ccbe4b517870e51ca93bb655c532cb765ee6b3b5
Yuliya-senkina <juliyasenkina@gmail.com>
2023-10-15 00:03:00 +0300

    commit

--- Calc/src/Main.java
+++ Calc/src/Main.java
@@ -10,22 +10,15 @@ public class Main {
         System.out.println("Multiply double = "+calc.multiply(3.5,5.6));
         System.out.println("Div double = "+ calc.div(3.5,5.6));
         System.out.println("Sum double+int = "+ calc.sum(1.5,3));
-<<<<<<< HEAD
-        
-=======
->>>>>>> e7f2c93 (commit)
         System.out.println("Sub double+int = "+ calc.sub(3.2,1));
         System.out.println("Multiply double+int = "+calc.multiply(3.5,5));
         System.out.println("Div double+int = "+ calc.div(3.5,5));
         System.out.println("Sum string+string = "+calc.sum("23","17"));
-<<<<<<< HEAD
         System.out.println("Sum string+string = "+calc.sub("23","17"));
         System.out.println("Sum string+string = "+calc.multiply("23","17"));
         System.out.println("Sum string+string = "+calc.div("23","17"));
-=======
         System.out.println("Sub string+string = "+calc.sub("23","17"));
         System.out.println("Multiply string+string = "+calc.multiply("23","17"));
         System.out.println("Div string+string = "+calc.div("23","17"));
->>>>>>> e7f2c93 (commit)
     }
 }
