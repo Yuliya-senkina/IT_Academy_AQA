a82dcb25c8db69ea1a615d3960403c9784a8fe0a
Yuliya-senkina <juliyasenkina@gmail.com>
2023-11-05 12:58:20 +0300

    commit

--- demo-selenium-web-driver/src/main/java/org/example/Main.java
+++ demo-selenium-web-driver/src/main/java/org/example/Main.java
@@ -7,10 +7,15 @@ import org.openqa.selenium.safari.SafariDriver;
 public class Main {
     public static void main(String[] args) {
         WebDriver driver= new SafariDriver();
-        driver.get("https://www.onliner.by");
-        String xpath="//*[@id=\"userbar\"]/div[1]/div/div/div[1]";
-        By xpathBY= By.xpath(xpath);
-        WebElement element = driver.findElement(xpathBY);
+        driver.get("https://svyatoslav.biz/testlab/wt/index.php");
+        // Input name
+        String inputNameXpath="/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
+        By inputNameXpathBY= By.xpath(inputNameXpath);
+        WebElement element = driver.findElement(inputNameXpathBY);
         element.sendKeys("Login");
+        String inputCalculateXpath="/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
+        By inputCalculateXpathBY= By.xpath(inputCalculateXpath);
+        WebElement element2 = driver.findElement(inputCalculateXpathBY);
+        element2.click();
         }
     }
