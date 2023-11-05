866e9f0a350ee183b0115c0c91a2fe87ed16450f
Yuliya-senkina <juliyasenkina@gmail.com>
2023-11-05 13:22:45 +0300

    commit

--- demo-selenium-web-driver/src/main/java/org/example/Main.java
+++ demo-selenium-web-driver/src/main/java/org/example/Main.java
@@ -11,11 +11,29 @@ public class Main {
         // Input name
         String inputNameXpath="/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[2]/td[2]/input";
         By inputNameXpathBY= By.xpath(inputNameXpath);
-        WebElement element = driver.findElement(inputNameXpathBY);
-        element.sendKeys("Login");
+        WebElement inputNameElement = driver.findElement(inputNameXpathBY);
+        inputNameElement.sendKeys("Login");
+        // Input height
+        String inputHeightXpath="/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[3]/td[2]/input";
+        By inputHeightXpathBY= By.xpath(inputHeightXpath);
+        WebElement inputHeightElement = driver.findElement(inputHeightXpathBY);
+        inputHeightElement.sendKeys("160");
+        // Input weight
+        String inputWeightXpath="/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[4]/td[2]/input";
+        By inputWeightXpathBY= By.xpath(inputWeightXpath);
+        WebElement inputWeightElement = driver.findElement(inputWeightXpathBY);
+        inputWeightElement.sendKeys("67");
+
+        // choose radio male/female
+        String inputGenderXpath="/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[5]/td[2]/input[1]";
+        By inputGenderXpathBY= By.xpath(inputGenderXpath);
+        WebElement inputGenderXpathElement = driver.findElement(inputGenderXpathBY);
+        inputGenderXpathElement.click();
+
+        // Put buttom calculte
         String inputCalculateXpath="/html/body/table/tbody/tr[2]/td[2]/form/table/tbody/tr[6]/td/input";
         By inputCalculateXpathBY= By.xpath(inputCalculateXpath);
-        WebElement element2 = driver.findElement(inputCalculateXpathBY);
-        element2.click();
+        WebElement inputCalculateElement = driver.findElement(inputCalculateXpathBY);
+        inputCalculateElement.click();
         }
     }
