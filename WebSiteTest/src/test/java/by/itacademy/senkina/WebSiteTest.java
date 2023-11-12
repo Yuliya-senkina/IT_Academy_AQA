395f756e87c4613bc51b918d86798a331db88ba8
Yuliya-senkina <juliyasenkina@gmail.com>
2023-11-12 11:27:46 +0300

    commit

--- WebSiteTest/src/test/java/by/itacademy/senkina/WebSiteTest.java
+++ WebSiteTest/src/test/java/by/itacademy/senkina/WebSiteTest.java
@@ -6,13 +6,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.safari.SafariDriver;
-
-import java.sql.Driver;
 
 public class WebSiteTest {
     @Test
-    public void testOnliner() {
+    public void testOnliner() throws InterruptedException {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.onliner.by");
@@ -20,12 +17,25 @@ public class WebSiteTest {
         By enterButtonBy = By.xpath(enterButtonLocator);
         WebElement enterButtonWebElement = driver.findElement(enterButtonBy);
         enterButtonWebElement.click();
-        String enterTitleFormLocator = "//*[@id=\"auth-container\"]/div/div[2]/div/div[1]";
-        By enterTitleFormBy = By.xpath(enterTitleFormLocator);
-        WebElement enterTitleFormElement = driver.findElement(enterTitleFormBy);
-        String actual = enterTitleFormElement.getText();
-        String expected = "Вход";
+
+        String buttonEnterLocator = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[3]/button";
+        By buttonEnterBy = By.xpath(buttonEnterLocator);
+        WebElement buttonEnterElement = driver.findElement(buttonEnterBy);
+        buttonEnterElement.click();
+        //Thread.sleep(4000);
+        String errorMessageLoginLocator = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[3]";
+        By errorMessageLoginBy = By.xpath(errorMessageLoginLocator);
+        WebElement errorMessageLoginElement = driver.findElement(errorMessageLoginBy);
+        String actual = errorMessageLoginElement.getText();
+        String expected = "Укажите ник или e-mail";
         Assertions.assertEquals(expected, actual);
+
+        String errorMessagePasswordLocator = "//*[@id=\"auth-container\"]/div/div[2]/div/form/div[2]/div/div/div[2]/div";
+        By errorMessagePasswordBy = By.xpath(errorMessagePasswordLocator);
+        WebElement errorMessagePasswordElement = driver.findElement(errorMessagePasswordBy);
+        String actualPassword = errorMessagePasswordElement.getText();
+        String expectedPassword = "Укажите пароль";
+        Assertions.assertEquals(expectedPassword, actualPassword);
     }
 
     @Test
@@ -33,17 +43,32 @@ public class WebSiteTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.navigate().to("https://devby.io");
+
         String enterButtonLocator = "/html/body/div[2]/header/div[1]/div[3]/a";
-        By enterButtonBy = By.xpath(enterButtonLocator);
-        WebElement enterButtonElement = driver.findElement(enterButtonBy);
-        enterButtonElement.click();
-        String enterTitleFormLocator = "//*[@id=\"root\"]/div/div/div/div/ul/a[1]";
-        By enterTitleFormBy = By.xpath(enterTitleFormLocator);
+        WebElement enterButton = driver.findElement(By.xpath(enterButtonLocator));
+        enterButton.click();
         Thread.sleep(4000);
-        WebElement enterTitleFormElement = driver.findElement(enterTitleFormBy);
-        String actual = enterTitleFormElement.getText();
-        String expected = "Вход";
+
+        String inputTextLoginLocator = "//*[@id=\"root\"]/div/div/div/div/div/div/form/ul/li[1]/input";
+        WebElement inputTextLogin = driver.findElement(By.xpath(inputTextLoginLocator));
+        inputTextLogin.sendKeys("webgetd");
+
+        String inputTextPasswordLocator = "//*[@id=\"root\"]/div/div/div/div/div/div/form/ul/li[2]/div/input";
+        WebElement inputTextPassword = driver.findElement(By.xpath(inputTextPasswordLocator));
+        inputTextPassword.sendKeys("321");
+
+        String enterButtonFormLocator = "//*[@id=\"root\"]/div/div/div/div/div/div/form/button";
+        WebElement enterButtonForm = driver.findElement(By.xpath(enterButtonFormLocator));
+        enterButtonForm.click();
+        Thread.sleep(4000);
+
+        String textErrorLoginPasswordLocator = "//*[@id=\"root\"]/div/div/div/div/div/div/form/span";
+        WebElement textErrorLoginPasswordElement = driver.findElement(By.xpath(textErrorLoginPasswordLocator));
+
+        String actual = textErrorLoginPasswordElement.getText();
+        String expected = "Неверный логин или пароль.";
         Assertions.assertEquals(expected, actual);
+        driver.quit();
     }
 
     @Test
